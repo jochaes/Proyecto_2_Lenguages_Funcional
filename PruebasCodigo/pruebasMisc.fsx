2ba579859fff47aa16014120e1af894116f64504
Josué Chaves Araya <d4n11083@estudiantec.cr>
2023-04-16 15:38:49 -0600

    Update pruebasMisc.fsx

--- PruebasCodigo/pruebasMisc.fsx
+++ PruebasCodigo/pruebasMisc.fsx
@@ -1,31 +1,24 @@
-let rec allCharsInWords chars words =
-  match chars with
-  | [] -> true
-  | h::t -> 
-      words |> List.exists (fun (w:string) -> w.Contains(h.ToString())) && allCharsInWords t words
 
-
-let  verificarPalabra (listaA: _ list) (listaB: _ list)  =
-  let A = List.sort listaA
-  let B = List.sort listaB
-  List.forall2 (fun elem1 elem2 -> elem1 = elem2) A B
-
-
-let checkWord (words:string list) (letters: char list) =
-  if (allCharsInWords letters words) then
-
-    if List.exists (fun (word:string) -> (verificarPalabra letters (Seq.toList word))) words then
-      printfn "Palabra Adivinada - los pinta de morado o algo"
-    else
-      printfn "Siga adivinando la palabra - Los deja en azul"
-  else 
-    printfn "Palabra Incorrecta - los pinta de nuevo de gris"
-    
-// printfn "%b" (allCharsInWords ['a'; 'b';'d';'c'] ["hello"; "abdc"] )
-
-// printfn "%b" (verificarPalabra ['a'; 'c';'b';'d'] (Seq.toList "abcd") )
+open System
+// let rec deleteString (str: string) (lst: string list) =
+//   match lst with
+//   | [] -> []
+//   | h::t when (Seq.sort h) = (Seq.sort str) -> t
+//   | h::t -> h :: deleteString str t
 
 
 
-checkWord ["hello"; "abdc"] ['a'; 'c';'d']
+// let myList = ["apple"; "banana"; "cherry"; "date"]
+// let updatedList = deleteString "banaan" myList
+// printfn "%A" updatedList // Output: ["apple"; "cherry"; "date"]
+  
+//printfn "%b" ( (Seq.sort "banana").ToString() = (Seq.sort "banaan").ToString() )
 
+let deleteString (strList : string list) (strToRemove : string) =
+  let removeStr = String.Concat( List.sort ( strToRemove.ToCharArray() |> List.ofArray ) )
+  strList |> List.filter (fun (s:string) -> String.Concat( List.sort ( s.ToCharArray() |> List.ofArray ) ) <> removeStr)
+  
+  
+let myList = ["apple"; "banana"; "cherry"; "date"]
+let updatedList = deleteString myList "abaann" 
+printfn "%A" updatedList // Output: ["apple"; "cherry"; "date"]
